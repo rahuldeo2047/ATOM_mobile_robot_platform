aac7099d4f8923a98b0444bd51081f24ababd6fc
rahuldeo2047 <rahuldeo2047@gmail.com>
2016-11-13 20:39:59 +0530

    Tested MPU6050 with the code. Filter Encoder+Acc Data with Rotation

---
+++ encoder_test/data.h
@@ -0,0 +1,15 @@
+
+#ifndef DATA_H
+#define DATA_H
+
+typedef struct true_angle_val_raw_acc
+{
+  float x_angle;
+  float y_angle;
+  float z_angle;
+  float x_unfiltered_acc;
+  float y_unfiltered_acc;
+  float z_unfiltered_acc;
+};
+
+#endif // DATA_H
